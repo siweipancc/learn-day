aeaf731a1dd4ab8d859ed3e290e424223c61776e
siweipancc <siweipancc@hotamail.com>
2024-10-20 22:37:23 +0800

    feat(C#): windows 交互

---
+++ CSharp/console/GetSelectFile/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GetSelectFile;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        var dialog = new FolderBrowserDialog();
+        var result = dialog.ShowDialog();
+        Console.WriteLine(result == DialogResult.OK ? $"选择的文件夹为: {dialog.SelectedPath}" : "请选择一个合法文件夹");
+
+        var openFileDialog = new OpenFileDialog();
+        var oResult = openFileDialog.ShowDialog();
+        Console.WriteLine(oResult == DialogResult.OK ? $"选择的文件为: {openFileDialog.FileName}" : "请打开一个合法文件");
+
+        Console.WriteLine("按任意键退出");
+        Console.ReadKey();
+    }
+}
